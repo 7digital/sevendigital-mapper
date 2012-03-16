c3e6ba37d0e26b02e8a4a22c47e506c0249f4f85
Paul Shannon <paul.shannon@7digital.com>
2012-03-16 14:34:21 +0000

    added search by 7dig id

--- src/SevenDigital.Mapper.Persistence.Unit.Tests/RepositoryTests.cs
+++ src/SevenDigital.Mapper.Persistence.Unit.Tests/RepositoryTests.cs
@@ -4,7 +4,7 @@ using NUnit.Framework;
 namespace SevenDigital.Mapper.Persistence.Unit.Tests
 {
     [TestFixture]
-    public class RepositoryTests
+    public class EmptyRepositoryTests
     {
         private Repository _subject;
         private List<IMapping> _loadedList;
@@ -25,8 +25,37 @@ namespace SevenDigital.Mapper.Persistence.Unit.Tests
         [Test]
         public void GetByEmptyMappingReturnsNullMapping()
         {
-
             Assert.That(_subject.GetBy(new Mapping()), Is.InstanceOf<NullMapping>());
+        }
+    }
+
+    [TestFixture]
+    public class RepositoryQueryTests
+    {
+        private Repository _subject;
+        private List<IMapping> _loadedList;
+        private SevenDigitalId _sevenDigitalId;
+        private Mapping _expectedMapping;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _sevenDigitalId = new SevenDigitalId(132456);
+            _expectedMapping = new Mapping 
+                                    {
+                                        SevenDigital = _sevenDigitalId
+                                    };
+            _loadedList = new List<IMapping> 
+                                {
+                                    _expectedMapping
+                                };
+            _subject = new Repository(_loadedList);
+        }
+
+        [Test]
+        public void GetByMappingWithSevenDigitalIdReturnsNullMapping()
+        {
+            Assert.That(_subject.GetBy(new Mapping { SevenDigital = _sevenDigitalId }), Is.SameAs(_expectedMapping));
         }
     }
 
--- src/SevenDigital.Mapper.Persistence/Repository.cs
+++ src/SevenDigital.Mapper.Persistence/Repository.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 
 namespace SevenDigital.Mapper.Persistence
 {
@@ -18,13 +19,76 @@ namespace SevenDigital.Mapper.Persistence
 
         public IMapping GetBy(Mapping mapping)
         {
-            return new NullMapping();
+            return _list.FirstOrDefault(m => m.Matches(mapping)) ?? new NullMapping();
         }
     }
 
-    public interface IMapping {}
+    public interface IMapping {
+        bool Matches(IMatchableMapping mapping);
+    }
 
-    public class NullMapping : IMapping { }
+    public class NullMapping : IMapping {
+        public bool Matches(IMatchableMapping mapping)
+        {
+            return false;
+        }
+    }
 
-    public class Mapping : IMapping { }
+    public interface IMatchableMapping {
+        SevenDigitalId SevenDigital { get; set; }
+    }
+
+    public class Mapping : IMapping, IMatchableMapping
+    {
+        public SevenDigitalId SevenDigital { get; set; }
+        public bool Matches(IMatchableMapping mapping)
+        {
+            return SevenDigital.Equals(mapping.SevenDigital);
+        }
+    }
+
+    public class SevenDigitalId
+    {
+        private readonly int _id;
+
+        public SevenDigitalId(int id)
+        {
+            _id = id;
+        }
+
+        public bool Equals(SevenDigitalId other)
+        {
+            if(ReferenceEquals(null, other))
+            {
+                return false;
+            }
+            if(ReferenceEquals(this, other))
+            {
+                return true;
+            }
+            return other._id == _id;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if(ReferenceEquals(null, obj))
+            {
+                return false;
+            }
+            if(ReferenceEquals(this, obj))
+            {
+                return true;
+            }
+            if(obj.GetType() != typeof(SevenDigitalId))
+            {
+                return false;
+            }
+            return Equals((SevenDigitalId) obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return _id;
+        }
+    }
 }
