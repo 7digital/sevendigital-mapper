5ac86373daeb9b04c41e7ee9ad3bce9d5bed93a3
Paul Shannon <paul.shannon@7digital.com>
2012-03-16 15:14:05 +0000

    refactored to use chain of responsibilty for matching

--- src/SevenDigital.Mapper.Domain/Mapping.cs
+++ src/SevenDigital.Mapper.Domain/Mapping.cs
@@ -2,8 +2,8 @@ namespace SevenDigital.Mapper.Domain
 {
     public class Mapping : IMapping, IMatchableMapping
     {
-        private readonly SevenDigitalId NULL_SEVEN_DIGITAL_ID = new SevenDigitalId(0);
-        private readonly MusicBrainzId NULL_MUSIC_BRAINZ_ID = new MusicBrainzId("");
+        public static readonly SevenDigitalId NULL_SEVEN_DIGITAL_ID = new SevenDigitalId(0);
+        public static readonly MusicBrainzId NULL_MUSIC_BRAINZ_ID = new MusicBrainzId("");
 
         public Mapping()
         {
@@ -17,12 +17,56 @@ namespace SevenDigital.Mapper.Domain
 
         public bool Matches(IMatchableMapping searchMapping)
         {
-            if (!searchMapping.SevenDigital.Equals(NULL_SEVEN_DIGITAL_ID))
-                return SevenDigital.Equals(searchMapping.SevenDigital);
-            if (!searchMapping.MusicBrainz.Equals(NULL_MUSIC_BRAINZ_ID))
-                return MusicBrainz.Equals(searchMapping.MusicBrainz);
+            return new SevenDigitalMatcher(new MusicBrainzMatcher(new NeverMatcher()))
+                .Match(this, searchMapping);
+        }
+    }
 
+    public class SevenDigitalMatcher : IMatcher
+    {
+        private readonly IMatcher _successor;
+
+        public SevenDigitalMatcher(IMatcher successor)
+        {
+            _successor = successor;
+        }
+
+        public bool Match(IMatchableMapping currentMapping, IMatchableMapping searchMapping)
+        {
+            if (!searchMapping.SevenDigital.Equals(Mapping.NULL_SEVEN_DIGITAL_ID))
+                return currentMapping.SevenDigital.Equals(searchMapping.SevenDigital);
+
+            return _successor.Match(currentMapping, searchMapping);
+        }
+    }
+
+    public class MusicBrainzMatcher : IMatcher
+    {
+        private readonly IMatcher _successor;
+
+        public MusicBrainzMatcher(IMatcher successor)
+        {
+            _successor = successor;
+        }
+
+        public bool Match(IMatchableMapping currentMapping, IMatchableMapping searchMapping)
+        {
+            if (!searchMapping.MusicBrainz.Equals(Mapping.NULL_MUSIC_BRAINZ_ID))
+                return currentMapping.MusicBrainz.Equals(searchMapping.MusicBrainz);
+
+            return _successor.Match(currentMapping, searchMapping);
+        }
+    }
+
+    public class NeverMatcher : IMatcher
+    {
+        public bool Match(IMatchableMapping currentMapping, IMatchableMapping searchMapping)
+        {
             return false;
         }
+    }
+
+    public interface IMatcher {
+        bool Match(IMatchableMapping currentMapping, IMatchableMapping searchMapping);
     }
 }
