b1ca035a9f7b597aecdfc4d06e9f342c94fa711a
Paul Shannon <paul.shannon@7digital.com>
2012-06-12 11:20:12 +0100

    simplified mapping module for testing the hosting

--- src/SevenDigital.Mapper.Api/MappingModule.cs
+++ src/SevenDigital.Mapper.Api/MappingModule.cs
@@ -9,26 +9,7 @@ namespace SevenDigital.Mapper.Api
     {
         public MappingModule()
         {
-            var mappingService = new MappingService(new LastFmLoader());
-            Get["/mbid/{Mbid}"] = req =>
-                                  {
-                                      var result = mappingService.Map(new Mapping { MusicBrainz = new MusicBrainzId(req.Mbid) });
-
-                                      return((Mapping) result).SevenDigital.ToString();
-                                  };
-
-            Get["/7did/{Id}"] = req =>
-            {
-                                    var result = mappingService.Map(new Mapping { SevenDigital = new SevenDigitalId(req.Id) });
-                                    return
-                                      Response.AsRedirect(
-                                          "http://www.last.fm/mbid/"
-                                          +
-                                          ((Mapping)result).MusicBrainz.ToString
-                                              ());
-                                                                       
-                                };
-			Get["/status"] = _ => DateTime.Now.ToString();
+			Get["/"] = _ => DateTime.Now.ToString();
         }
 		
     }
